4b622b4a3ea69b6f029c24423f0b4c8447f33608
hotcheetoqueen <jessica@userinterviews.com>
2021-03-18 22:30:25 -0700

    fix(auth): add authorization middleware to user routes

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
 const users = require('../controllers/users');
-const { validateId } = require('../middlewares/validator');
-// const auth = require('./middlewares/auth');
+const auth = require('../middlewares/auth');
 
-// app.use(auth);
-
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
   users.getUsers(req, res);
 });
 
-router.get('/me', validateId, (req, res) => {
+router.get('/me', auth, (req, res) => {
   users.getCurrentUser(req, res);
 });
 
